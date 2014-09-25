4fa818035ac35c49f7a540de5c5ef219811f59ec
ludovic-decampy <ludovic.decampy@gmail.com>
2014-09-25 18:42:49 +0200

    Updated README.md

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-html4-history-hash-original-url
-===============================
+redirect.js 
+=========
 
-Retrieve the url hidden behing and HTML4 history hash glitch
+redirect.js is a script that permit to retrieve the url hidden behing and HTML4 history hash glitch
+
+Installation
+--------------
+
+Put the redirect.js file wherever you want in your project and link it in your html document
+
+```
+<script src="path/to/redirect.js" type="text/javascript"></script>
+```
+
+
+**Enjoy :)**
+
---
+++ redirect.js
@@ -0,0 +1,23 @@
+/**
+* redirect.js 
+*
+* Retrieve the url hidden behing and HTML4 history hash glitch
+*
+* @author Ludovic Decampy <ludovic.decampy@gmail.com>
+* @copyright 2014 Ludovic Decampy <ludovic.decampy@gmail.com>
+* @license MIT License <http://creativecommons.org/licenses/MIT/>
+*/
+
+(function(){
+    if(window.location.hash !== ''){
+        var a = window.location.href; 
+        //Replace root path 
+        var b = a.replace(/^(http:\/\/[^\/]*\/)[^\#]*\#\/(.*)$/,'$1$2');
+        //Replace relative path 
+        b = b.replace(/^(http:\/\/[^\/]*\/[^\#]*\/)[^\#]*\#(\.\/)?(.*)$/,'$1$3');
+        if(b !== a){
+            // Do whatever during the redirection ...
+            window.location = b;
+        }
+    }
+})();
